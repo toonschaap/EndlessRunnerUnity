28dce8afe6790b26ea1cad98a6d42fbe1c113a97
Eigenaar <toonschaap3@gmail.com>
2018-10-11 11:46:26 +0200

    final

---
+++ Assets/scripts/Player/Constants.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public class Constants : MonoBehaviour
+ {
+     public const int NUMBER_OF_PLAYERS = 1;
+	 
+ }
---
+++ Assets/scripts/scenechangers/slowdown.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class slowdown : MonoBehaviour
+{
+	public void NextScene()
+	{
+		SceneManager.LoadScene("Slow");
+	}
+}
